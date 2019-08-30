04047146ac0a37290234020fa613bc684efd853f
Ana Caceres <anadelorenzoCL@gmail.com>
2019-08-30 22:22:11 +0200

    Changing server & .travis.yml config

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const express = require("express");
-const port = process.env.PORT || 8080;
-const app = express();
+let express = require('express');
+let port = process.env.PORT || 8080;
+let serveStatic = require('serve-static');
 
-app.use(express.static(__dirname + "/dist/"));
-app.get(/.*/, function(req, res) {
-  res.sendfile(__dirname + "/dist/index.html");
-});
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+
 app.listen(port);
