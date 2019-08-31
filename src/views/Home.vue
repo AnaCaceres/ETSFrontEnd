6212a29473d3e935bac760578b7714a1dbad296a
Ana Caceres <anadelorenzoCL@gmail.com>
2019-08-31 18:22:00 +0200

    Fetching the data from the API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to ETS Front-End First Deploy" />
-  </div>
+  <div class="home">{{ actives }}</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      actives: []
+    };
+  },
+  methods: {
+    fetchActives() {
+      fetch("http://jsonstub.com/symbols", {
+        method: "GET",
+        headers: new Headers({
+          "Content-Type": "application/json",
+          "JsonStub-User-Key": "6fe1470b-4310-4998-bc11-969c53fd512e",
+          "JsonStub-Project-Key": "31597c40-92cb-429a-8394-9cff99a51925"
+        })
+      })
+        .then(stream => stream.json())
+        .then(data => (this.actives = data))
+        .catch(error => console.error(error));
+    }
+  },
+  mounted(){
+    this.fetchActives();
   }
 };
 </script>
